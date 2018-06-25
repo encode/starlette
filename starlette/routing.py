2a4955f9939044cea59bd4ceda656fb16e970cc9
Tom Christie <tom@tomchristie.com>
2018-06-25 22:26:01 +0100

    Black

--- starlette/routing.py
+++ starlette/routing.py
@@ -6,17 +6,17 @@ class Path:
     def __init__(self, path, app):
         self.path = path
         self.app = app
-        regex = '^' + path + '$'
-        regex = re.sub('{([a-zA-Z_][a-zA-Z0-9_]*)}', r'(?P<\1>[^/]*)', regex)
+        regex = "^" + path + "$"
+        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]*)", regex)
         self.path_regex = re.compile(regex)
 
     def matches(self, scope):
-        match = self.path_regex.match(scope['path'])
+        match = self.path_regex.match(scope["path"])
         if match:
-            kwargs = dict(scope.get('kwargs', {}))
+            kwargs = dict(scope.get("kwargs", {}))
             kwargs.update(match.groupdict())
             child_scope = scope.copy()
-            child_scope['kwargs'] = kwargs
+            child_scope["kwargs"] = kwargs
             return True, child_scope
         return False, {}
 
@@ -28,19 +28,19 @@ class PathPrefix:
     def __init__(self, path, app):
         self.path = path
         self.app = app
-        regex = '^' + path
-        regex = re.sub('{([a-zA-Z_][a-zA-Z0-9_]*)}', r'(?P<\1>[^/]*)', regex)
+        regex = "^" + path
+        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]*)", regex)
         self.path_regex = re.compile(regex)
 
     def matches(self, scope):
-        match = self.path_regex.match(scope['path'])
+        match = self.path_regex.match(scope["path"])
         if match:
-            kwargs = dict(scope.get('kwargs', {}))
+            kwargs = dict(scope.get("kwargs", {}))
             kwargs.update(match.groupdict())
             child_scope = scope.copy()
-            child_scope['kwargs'] = kwargs
-            child_scope['root_path'] = scope.get('root_path', '') + match.string
-            child_scope['path'] = scope['path'][match.span()[1]:]
+            child_scope["kwargs"] = kwargs
+            child_scope["root_path"] = scope.get("root_path", "") + match.string
+            child_scope["path"] = scope["path"][match.span()[1] :]
             return True, child_scope
         return False, {}
 
@@ -61,4 +61,4 @@ class Router:
         return self.not_found(scope)
 
     def not_found(self, scope):
-        return Response('Not found', 404, media_type='text/plain')
+        return Response("Not found", 404, media_type="text/plain")
