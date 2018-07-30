853e7cb9f879f4feaa45f6832955a39f338c70e9
erm <jermff@gmail.com>
2018-07-30 19:59:00 +1000

    Refactor view to allow both sync/async methods

--- starlette/views.py
+++ starlette/views.py
@@ -4,20 +4,20 @@ from starlette.decorators import make_asgi
 
 
 class View:
-    def dispatch(self, request):
-        request_method = request.method if request.method != "HEAD" else "GET"
+    def dispatch(self, scope):
+        request_method = scope["method"] if scope["method"] != "HEAD" else "GET"
         func = getattr(self, request_method.lower(), None)
         if func is None:
             raise Exception(
                 f"Method {request_method} is not implemented for this view."
             )
-        return func(request)
+        return make_asgi(func)(scope)
 
     @classmethod
     def as_view(cls) -> typing.Callable:
         def view(scope):
             self = cls()
-            return make_asgi(self.dispatch)(scope)
+            return self.dispatch(scope)
 
         view.view_class = cls
         update_wrapper(view, cls, updated=())
