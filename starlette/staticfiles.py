2a3f48fc3356fd5c7253eb00b83e83a484bc5b8a
Marcos Schroh <schrohm@gmail.com>
2018-09-25 15:01:33 +0200

    fix(Missing annotation): missing annotations added on staticfiles module.

--- starlette/staticfiles.py
+++ starlette/staticfiles.py
@@ -1,25 +1,33 @@
-from starlette.responses import PlainTextResponse, FileResponse
-from aiofiles.os import stat as aio_stat
 import os
 import stat
+import typing
+
+from aiofiles.os import stat as aio_stat
+
+from starlette.responses import PlainTextResponse, FileResponse
+from starlette.types import Send, Receive, Scope
 
 
 class StaticFile:
-    def __init__(self, *, path):
+    def __init__(self, *, path: str) -> None:
         self.path = path
 
-    def __call__(self, scope):
+    def __call__(
+        self, scope: Scope
+    ) -> typing.Union[PlainTextResponse, "_StaticFileResponder"]:
         if scope["method"] not in ("GET", "HEAD"):
             return PlainTextResponse("Method Not Allowed", status_code=405)
         return _StaticFileResponder(scope, path=self.path)
 
 
 class StaticFiles:
-    def __init__(self, *, directory):
+    def __init__(self, *, directory: str) -> None:
         self.directory = directory
         self.config_checked = False
 
-    def __call__(self, scope):
+    def __call__(
+        self, scope: Scope
+    ) -> typing.Union[PlainTextResponse, "_StaticFilesResponder"]:
         if scope["method"] not in ("GET", "HEAD"):
             return PlainTextResponse("Method Not Allowed", status_code=405)
         path = os.path.normpath(os.path.join(*scope["path"].split("/")))
@@ -35,11 +43,11 @@ class StaticFiles:
 
 
 class _StaticFileResponder:
-    def __init__(self, scope, path):
+    def __init__(self, scope: Scope, path: str) -> None:
         self.scope = scope
         self.path = path
 
-    async def __call__(self, receive, send):
+    async def __call__(self, receive: Receive, send: Send) -> None:
         try:
             stat_result = await aio_stat(self.path)
         except FileNotFoundError:
@@ -54,12 +62,14 @@ class _StaticFileResponder:
 
 
 class _StaticFilesResponder:
-    def __init__(self, scope, path, check_directory=None):
+    def __init__(
+        self, scope: Scope, path: str, check_directory: typing.Optional[str] = None
+    ) -> None:
         self.scope = scope
         self.path = path
         self.check_directory = check_directory
 
-    async def check_directory_configured_correctly(self):
+    async def check_directory_configured_correctly(self) -> None:
         """
         Perform a one-off configuration check that StaticFiles is actually
         pointed at a directory, so that we can raise loud errors rather than
@@ -73,19 +83,21 @@ class _StaticFilesResponder:
         if not (stat.S_ISDIR(stat_result.st_mode) or stat.S_ISLNK(stat_result.st_mode)):
             raise RuntimeError("StaticFiles path '%s' is not a directory." % directory)
 
-    async def __call__(self, receive, send):
+    async def __call__(self, receive: Receive, send: Send) -> None:
         if self.check_directory is not None:
             await self.check_directory_configured_correctly()
 
         try:
             stat_result = await aio_stat(self.path)
         except FileNotFoundError:
-            response = PlainTextResponse("Not Found", status_code=404)
+            response = PlainTextResponse(
+                "Not Found", status_code=404
+            )  # type: typing.Union[PlainTextResponse, FileResponse]
         else:
             mode = stat_result.st_mode
+
+            response = FileResponse(self.path, stat_result=stat_result)
             if not stat.S_ISREG(mode):
                 response = PlainTextResponse("Not Found", status_code=404)
-            else:
-                response = FileResponse(self.path, stat_result=stat_result)
 
         await response(receive, send)
