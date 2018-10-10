bbbc55e2e66427065cc508296ab4ea889d4edce5
Tom Christie <tom@tomchristie.com>
2018-10-10 10:39:44 +0100

    Add test for multipart files

--- tests/test_multipart.py
+++ tests/test_multipart.py
@@ -26,3 +26,24 @@ def test_multipart_request_data(tmpdir):
     client = TestClient(app)
     response = client.post("/", data={"some": "data"}, files=FORCE_MULTIPART)
     assert response.json() == {"form": {"some": "data"}}
+
+
+def test_multipart_request_files(tmpdir):
+    def app(scope):
+        async def asgi(receive, send):
+            request = Request(scope, receive)
+            data = {"form": await request.form()}
+            response = JSONResponse(data)
+            await response(receive, send)
+
+        return asgi
+
+    path = os.path.join(tmpdir, "test.txt")
+    with open(path, "wb") as file:
+        file.write(b"<file content>")
+
+    client = TestClient(app)
+    response = client.post("/", files={"test": open(path, "rb")})
+    assert response.json() == {
+        "form": {"test": {"name": "test.txt", "stream": ["<file content>"]}}
+    }
