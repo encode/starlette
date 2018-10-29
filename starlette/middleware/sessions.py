87da226f1c1134c2c3a1b45dc01780ded306a70e
Tom Christie <tom@tomchristie.com>
2018-10-29 16:16:51 +0000

    Session middleware (#157)
    * Use isort for consistent import ordering
    
    * Add SessionMmiddleware support
    
    * Add SessionMiddleware support

---
+++ starlette/middleware/sessions.py
@@ -0,0 +1,44 @@
+import functools
+import json
+from base64 import b64decode, b64encode
+
+import itsdangerous
+
+from starlette.datastructures import MutableHeaders
+from starlette.requests import Request
+from starlette.types import ASGIApp, ASGIInstance, Message, Receive, Scope, Send
+
+
+class SessionMiddleware:
+    def __init__(
+        self, app: ASGIApp, secret_key: str, session_cookie: str = "session"
+    ) -> None:
+        self.app = app
+        self.signer = itsdangerous.Signer(secret_key)
+        self.session_cookie = session_cookie
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        if scope["type"] in ("http", "websocket"):
+            request = Request(scope)
+            if self.session_cookie in request.cookies:
+                data = request.cookies[self.session_cookie].encode("utf-8")
+                data = self.signer.unsign(data)
+                scope["session"] = json.loads(b64decode(data))
+            else:
+                scope["session"] = {}
+            return functools.partial(self.asgi, scope=scope)
+        return self.app(scope)  # pragma: no cover
+
+    async def asgi(self, receive: Receive, send: Send, scope: Scope) -> None:
+        inner = self.app(scope)
+
+        async def sender(message: Message) -> None:
+            if message["type"] == "http.response.start" and scope["session"]:
+                data = b64encode(json.dumps(scope["session"]).encode("utf-8"))
+                data = self.signer.sign(data)
+                headers = MutableHeaders(scope=message)
+                header_value = "%s=%s" % (self.session_cookie, data.decode("utf-8"))
+                headers.append("Set-Cookie", header_value)
+            await send(message)
+
+        await inner(receive, sender)
