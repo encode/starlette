882f5a2c4bde77cf63e07ba0431c9836322211f4
laixintao <nanshi.lxt@alipay.com>
2018-11-02 22:18:22 +0800

    restore aiofiles'open after testcase

--- tests/test_staticfiles.py
+++ tests/test_staticfiles.py
@@ -100,14 +100,17 @@ def test_staticfiles_prevents_breaking_out_of_directory(tmpdir):
     assert response.body == b"Not Found"
 
 
-def test_staticfiles_head(tmpdir):
+def test_staticfiles_never_read_file_for_head_method(tmpdir):
     path = os.path.join(tmpdir, "example.txt")
     with open(path, "w") as file:
         file.write("<file content>")
     import aiofiles
+    old_aiofiles = aiofiles.open
     aiofiles.open = None
 
     app = StaticFiles(directory=tmpdir)
     client = TestClient(app)
     response = client.head("/example.txt")
     assert response.status_code == 200
+    assert response.headers["content-length"] == "14"
+    aiofiles.open = old_aiofiles
