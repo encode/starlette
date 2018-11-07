1a9b87318d5c632e4cb53ea16ba7662081b633c8
Tom Christie <tom@tomchristie.com>
2018-11-07 13:35:29 +0000

    Fix contextvars support with threadpools

--- starlette/concurrency.py
+++ starlette/concurrency.py
@@ -4,7 +4,7 @@ import typing
 
 try:
     import contextvars  # Python 3.7+ only.
-except ImportError:
+except ImportError:  # pragma: no cover
     contextvars = None  # type: ignore
 
 
@@ -14,10 +14,11 @@ async def run_in_threadpool(
     loop = asyncio.get_event_loop()
     if contextvars is not None:  # pragma: no cover
         # Ensure we run in the same context
+        child = functools.partial(func, *args, **kwargs)
         context = contextvars.copy_context()
-        func = functools.partial(context.run, *args, **kwargs)
-        args = ()
-    elif kwargs:
+        func = context.run
+        args = (child,)
+    elif kwargs:  # pragma: no cover
         # loop.run_in_executor doesn't accept 'kwargs', so bind them in here
         func = functools.partial(func, **kwargs)
     return await loop.run_in_executor(None, func, *args)
