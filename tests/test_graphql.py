9e7e21d0191bccf3b1486b99fd9de86033deed00
Pascal Maximilian Bremer <8161919+Bomberus@users.noreply.github.com>
2018-11-12 17:14:18 +0100

    Update test_graphql.py

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -110,14 +110,6 @@ def test_graphql_async():
 
 class Upload(graphene.types.Scalar):
     @staticmethod
-    def serialize(value):
-        return value
-
-    @staticmethod
-    def parse_literal(node):
-        return node
-
-    @staticmethod
     def parse_value(value):
         return value
 
