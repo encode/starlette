be7b88ecec68c8188f22f9ca0c94ad91ac128b5f
Tom Christie <tom@tomchristie.com>
2018-11-30 14:29:10 +0000

    Type annotations, linting, and minor package tweaks

--- starlette/middleware/database.py
+++ starlette/middleware/database.py
@@ -1,48 +1,56 @@
-import asyncpgsa
 import functools
+import typing
+
+import asyncpgsa
+
+from starlette.types import ASGIApp, ASGIInstance, Message, Receive, Scope, Send
 
 
 class DatabaseMiddleware:
-    def __init__(self, app, database_url):
+    def __init__(self, app: ASGIApp, database_url: str) -> None:
         self.app = app
         self.database_url = database_url
-        self.pool = None
+        self.pool = None  # type: typing.Any
 
-    def __call__(self, scope):
-        if scope['type'] == 'lifespan':
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        if scope["type"] == "lifespan":
             return DatabaseLifespan(self.app, self, scope)
         return functools.partial(self.asgi, scope=scope)
 
-    async def asgi(self, receive, send, scope=None):
+    async def asgi(self, receive: Receive, send: Send, scope: Scope) -> None:
         conn = await self.pool.acquire()
         try:
-            scope['db'] = conn
+            scope["db"] = conn
             inner = self.app(scope)
             await inner(receive, send)
         finally:
             await conn.close()
 
-    async def startup(self):
+    async def startup(self) -> None:
         self.pool = await asyncpgsa.create_pool(self.database_url)
 
-    async def shutdown(self):
+    async def shutdown(self) -> None:
         await self.pool.close()
 
 
 class DatabaseLifespan:
-    def __init__(self, app, middleware, scope):
+    def __init__(
+        self, app: ASGIApp, middleware: DatabaseMiddleware, scope: Scope
+    ) -> None:
         self.inner = app(scope)
         self.middleware = middleware
 
-    async def __call__(self, receive, send):
+    async def __call__(self, receive: Receive, send: Send) -> None:
         try:
-            async def receiver():
+
+            async def receiver() -> Message:
                 message = await receive()
-                if message['type'] == 'lifespan.startup':
+                if message["type"] == "lifespan.startup":
                     await self.middleware.startup()
-                elif message['type'] == 'lifespan.shutdown':
+                elif message["type"] == "lifespan.shutdown":
                     await self.middleware.shutdown()
                 return message
+
             await self.inner(receiver, send)
         finally:
-            self.middleware = None
+            self.middleware = None  # type: ignore
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,57 +1,68 @@
+import os
+
+import pytest
 import sqlalchemy
+
 from starlette.applications import Starlette
 from starlette.middleware.database import DatabaseMiddleware
 from starlette.responses import JSONResponse
 from starlette.testclient import TestClient
-import os
-import pytest
 
-DATABASE_URL = os.environ['STARLETTE_TEST_DATABASE']
+DATABASE_URL = os.environ["STARLETTE_TEST_DATABASE"]
 
 metadata = sqlalchemy.MetaData()
 
-notes = sqlalchemy.Table('notes', metadata,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('text', sqlalchemy.String),
-    sqlalchemy.Column('completed', sqlalchemy.Boolean),
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
 )
 
 app = Starlette()
 app.add_middleware(DatabaseMiddleware, database_url=DATABASE_URL)
+
 
 @pytest.fixture(autouse=True, scope="module")
 def create_test_database():
     engine = sqlalchemy.create_engine(DATABASE_URL)
     metadata.create_all(engine)
     yield
-    engine.execute('DROP TABLE notes')
+    engine.execute("DROP TABLE notes")
 
 
-@app.route('/notes', methods=['GET'])
+@app.route("/notes", methods=["GET"])
 async def list_notes(request):
     query = sqlalchemy.select([notes])
     results = await request.db.fetch(query)
-    content = [{'text': result['text'], 'completed': result['completed']} for result in results]
+    content = [
+        {"text": result["text"], "completed": result["completed"]} for result in results
+    ]
     return JSONResponse(content)
 
 
-@app.route('/notes', methods=['POST'])
+@app.route("/notes", methods=["POST"])
 async def add_note(request):
     data = await request.json()
-    query = notes.insert().values(text=data['text'], completed=data['completed'])
+    query = notes.insert().values(text=data["text"], completed=data["completed"])
     await request.db.execute(query)
-    return JSONResponse({'text': data['text'], 'completed': data['completed']})
+    return JSONResponse({"text": data["text"], "completed": data["completed"]})
 
 
 def test_database():
     with TestClient(app) as client:
-        response = client.post('/notes', json={'text': 'buy the milk', 'completed': True})
+        response = client.post(
+            "/notes", json={"text": "buy the milk", "completed": True}
+        )
         assert response.status_code == 200
-        response = client.post('/notes', json={'text': 'walk the dog', 'completed': False})
+        response = client.post(
+            "/notes", json={"text": "walk the dog", "completed": False}
+        )
         assert response.status_code == 200
-        response = client.get('/notes')
+        response = client.get("/notes")
         assert response.status_code == 200
         assert response.json() == [
-            {'text': 'buy the milk', 'completed': True},
-            {'text': 'walk the dog', 'completed': False}
+            {"text": "buy the milk", "completed": True},
+            {"text": "walk the dog", "completed": False},
         ]
