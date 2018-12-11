encode/starlette

commit 14650bfd221b7d4236234bd364d427c35d2b33b6

URL convertors (#221)
* Version 0.8.4

* Add support for path convertors

* Add to_string on convertors

starlette/routing.py
@@ -6,6 +6,7 @@ from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
 
 from starlette.concurrency import run_in_threadpool
+from starlette.convertors import CONVERTOR_TYPES, Convertor
 from starlette.datastructures import URL, URLPath
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
@@ -73,13 +74,20 @@ def get_name(endpoint: typing.Callable) -> str:
 
 
 def replace_params(
-    path: str, path_params: typing.Dict[str, str]
+    path: str,
+    param_convertors: typing.Dict[str, Convertor],
+    path_params: typing.Dict[str, str],
 ) -> typing.Tuple[str, dict]:
     for key, value in list(path_params.items()):
         if "{" + key + "}" in path:
-            path_params.pop(key)
+            convertor = param_convertors[key]
+            value = convertor.to_string(value)
             path = path.replace("{" + key + "}", value)
+            path_params.pop(key)
     return path, path_params
+
+
+PARAM_REGEX = re.compile("{([a-zA-Z_][a-zA-Z0-9_]*)(:[a-zA-Z_][a-zA-Z0-9_]*)?}")
 
 
 class BaseRoute:
@@ -91,6 +99,45 @@ class BaseRoute:
 
     def __call__(self, scope: Scope) -> ASGIInstance:
         raise NotImplementedError()  # pragma: no cover
+
+    def compile_path(
+        self, path: str
+    ) -> typing.Tuple[typing.Pattern, str, typing.Dict[str, Convertor]]:
+        """
+        Given a path string, like: "/{username:str}", return a three-tuple
+        of (regex, format, {param_name:convertor}).
+
+        regex:      "/(?P<username>[^/]+)"
+        format:     "/{username}"
+        convertors: {"username": StringConvertor()}
+        """
+        path_regex = "^"
+        path_format = ""
+
+        idx = 0
+        param_convertors = {}
+        for match in PARAM_REGEX.finditer(path):
+            param_name, convertor_type = match.groups("str")
+            convertor_type = convertor_type.lstrip(":")
+            assert convertor_type in CONVERTOR_TYPES, (
+                "Unknown path convertor '%s'" % convertor_type
+            )
+            convertor = CONVERTOR_TYPES[convertor_type]
+
+            path_regex += path[idx : match.start()]
+            path_regex += "(?P<%s>%s)" % (param_name, convertor.regex)
+
+            path_format += path[idx : match.start()]
+            path_format += "{%s}" % param_name
+
+            param_convertors[param_name] = convertor
+
+            idx = match.end()
+
+        path_regex += path[idx:] + "$"
+        path_format += path[idx:]
+
+        return re.compile(path_regex), path_format, param_convertors
 
 
 class Route(BaseRoute):
@@ -119,17 +166,19 @@ class Route(BaseRoute):
             self.app = endpoint
 
         self.methods = methods
-        regex = "^" + path + "$"
-        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]+)", regex)
-        self.path_regex = re.compile(regex)
-        self.param_names = set(self.path_regex.groupindex.keys())
+        self.path_regex, self.path_format, self.param_convertors = self.compile_path(
+            path
+        )
 
     def matches(self, scope: Scope) -> typing.Tuple[Match, Scope]:
         if scope["type"] == "http":
             match = self.path_regex.match(scope["path"])
             if match:
+                matched_params = match.groupdict()
+                for key, value in matched_params.items():
+                    matched_params[key] = self.param_convertors[key].convert(value)
                 path_params = dict(scope.get("path_params", {}))
-                path_params.update(match.groupdict())
+                path_params.update(matched_params)
                 child_scope = {"endpoint": self.endpoint, "path_params": path_params}
                 if self.methods and scope["method"] not in self.methods:
                     return Match.PARTIAL, child_scope
@@ -138,9 +187,15 @@ class Route(BaseRoute):
         return Match.NONE, {}
 
     def url_path_for(self, name: str, **path_params: str) -> URLPath:
-        if name != self.name or self.param_names != set(path_params.keys()):
+        seen_params = set(path_params.keys())
+        expected_params = set(self.param_convertors.keys())
+
+        if name != self.name or seen_params != expected_params:
             raise NoMatchFound()
-        path, remaining_params = replace_params(self.path, path_params)
+
+        path, remaining_params = replace_params(
+            self.path_format, self.param_convertors, path_params
+        )
         assert not remaining_params
         return URLPath(path=path, protocol="http")
 
@@ -178,23 +233,33 @@ class WebSocketRoute(BaseRoute):
 
         regex = "^" + path + "$"
         regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]+)", regex)
-        self.path_regex = re.compile(regex)
-        self.param_names = set(self.path_regex.groupindex.keys())
+        self.path_regex, self.path_format, self.param_convertors = self.compile_path(
+            path
+        )
 
     def matches(self, scope: Scope) -> typing.Tuple[Match, Scope]:
         if scope["type"] == "websocket":
             match = self.path_regex.match(scope["path"])
             if match:
+                matched_params = match.groupdict()
+                for key, value in matched_params.items():
+                    matched_params[key] = self.param_convertors[key].convert(value)
                 path_params = dict(scope.get("path_params", {}))
-                path_params.update(match.groupdict())
+                path_params.update(matched_params)
                 child_scope = {"endpoint": self.endpoint, "path_params": path_params}
                 return Match.FULL, child_scope
         return Match.NONE, {}
 
     def url_path_for(self, name: str, **path_params: str) -> URLPath:
-        if name != self.name or self.param_names != set(path_params.keys()):
+        seen_params = set(path_params.keys())
+        expected_params = set(self.param_convertors.keys())
+
+        if name != self.name or seen_params != expected_params:
             raise NoMatchFound()
-        path, remaining_params = replace_params(self.path, path_params)
+
+        path, remaining_params = replace_params(
+            self.path_format, self.param_convertors, path_params
+        )
         assert not remaining_params
         return URLPath(path=path, protocol="websocket")
 
@@ -214,10 +279,10 @@ class Mount(BaseRoute):
         assert path == "" or path.startswith("/"), "Routed paths must always start '/'"
         self.path = path.rstrip("/")
         self.app = app
-        regex = "^" + self.path + "(?P<path>/.*)$"
-        regex = re.sub("{([a-zA-Z_][a-zA-Z0-9_]*)}", r"(?P<\1>[^/]*)", regex)
-        self.path_regex = re.compile(regex)
         self.name = name
+        self.path_regex, self.path_format, self.param_convertors = self.compile_path(
+            path + "/{path:path}"
+        )
 
     @property
     def routes(self) -> typing.List[BaseRoute]:
@@ -228,7 +293,9 @@ class Mount(BaseRoute):
         match = self.path_regex.match(path)
         if match:
             matched_params = match.groupdict()
-            remaining_path = matched_params.pop("path")
+            for key, value in matched_params.items():
+                matched_params[key] = self.param_convertors[key].convert(value)
+            remaining_path = "/" + matched_params.pop("path")
             matched_path = path[: -len(remaining_path)]
             path_params = dict(scope.get("path_params", {}))
             path_params.update(matched_params)
@@ -245,7 +312,9 @@ class Mount(BaseRoute):
         if self.name is not None and name == self.name and "path" in path_params:
             # 'name' matches "<mount_name>".
             path_params["path"] = path_params["path"].lstrip("/")
-            path, remaining_params = replace_params(self.path + "/{path}", path_params)
+            path, remaining_params = replace_params(
+                self.path_format, self.param_convertors, path_params
+            )
             if not remaining_params:
                 return URLPath(path=path, protocol="http")
         elif self.name is None or name.startswith(self.name + ":"):
@@ -255,11 +324,16 @@ class Mount(BaseRoute):
             else:
                 # 'name' matches "<mount_name>:<child_name>".
                 remaining_name = name[len(self.name) + 1 :]
-            path, remaining_params = replace_params(self.path, path_params)
+            path_params["path"] = ""
+            path, remaining_params = replace_params(
+                self.path_format, self.param_convertors, path_params
+            )
             for route in self.routes or []:
                 try:
                     url = route.url_path_for(remaining_name, **remaining_params)
-                    return URLPath(path=path + str(url), protocol=url.protocol)
+                    return URLPath(
+                        path=path.rstrip("/") + str(url), protocol=url.protocol
+                    )
                 except NoMatchFound as exc:
                     pass
         raise NoMatchFound()

tests/test_routing.py
@@ -1,7 +1,7 @@
 import pytest
 
 from starlette.exceptions import ExceptionMiddleware
-from starlette.responses import PlainTextResponse, Response
+from starlette.responses import JSONResponse, PlainTextResponse, Response
 from starlette.routing import Mount, NoMatchFound, Route, Router, WebSocketRoute
 from starlette.testclient import TestClient
 from starlette.websockets import WebSocket, WebSocketDisconnect
@@ -48,6 +48,24 @@ def contact(request):
     return Response("Hello, POST!", media_type="text/plain")
 
 
+@app.route("/int/{param:int}", name="int-convertor")
+def int_convertor(request):
+    number = request.path_params["param"]
+    return JSONResponse({"int": number})
+
+
+@app.route("/float/{param:float}", name="float-convertor")
+def float_convertor(request):
+    num = request.path_params["param"]
+    return JSONResponse({"float": num})
+
+
+@app.route("/path/{param:path}", name="path-convertor")
+def path_convertor(request):
+    path = request.path_params["param"]
+    return JSONResponse({"path": path})
+
+
 @app.websocket_route("/ws")
 async def websocket_endpoint(session):
     await session.accept()
@@ -91,12 +109,38 @@ def test_router():
     assert response.text == "xxxxx"
 
 
+def test_route_converters():
+    # Test integer conversion
+    response = client.get("/int/5")
+    assert response.status_code == 200
+    assert response.json() == {"int": 5}
+    assert app.url_path_for("int-convertor", param=5) == "/int/5"
+
+    # Test float conversion
+    response = client.get("/float/25.5")
+    assert response.status_code == 200
+    assert response.json() == {"float": 25.5}
+    assert app.url_path_for("float-convertor", param=25.5) == "/float/25.5"
+
+    # Test path conversion
+    response = client.get("/path/some/example")
+    assert response.status_code == 200
+    assert response.json() == {"path": "some/example"}
+    assert (
+        app.url_path_for("path-convertor", param="some/example") == "/path/some/example"
+    )
+
+
 def test_url_path_for():
     assert app.url_path_for("homepage") == "/"
     assert app.url_path_for("user", username="tomchristie") == "/users/tomchristie"
     assert app.url_path_for("websocket_endpoint") == "/ws"
     with pytest.raises(NoMatchFound):
         assert app.url_path_for("broken")
+    with pytest.raises(AssertionError):
+        app.url_path_for("user", username="tom/christie")
+    with pytest.raises(AssertionError):
+        app.url_path_for("user", username="")
 
 
 def test_url_for():

starlette/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.8.3"
+__version__ = "0.8.4"

starlette/convertors.py
@@ -1,0 +1,69 @@
+import math
+import typing
+
+
+class Convertor:
+    regex = ""
+
+    def convert(self, value: str) -> typing.Any:
+        raise NotImplementedError()  # pragma: no cover
+
+    def to_string(self, value: typing.Any) -> str:
+        raise NotImplementedError()  # pragma: no cover
+
+
+class StringConvertor(Convertor):
+    regex = "[^/]+"
+
+    def convert(self, value: str) -> typing.Any:
+        return value
+
+    def to_string(self, value: typing.Any) -> str:
+        value = str(value)
+        assert "/" not in value, "May not contain path seperators"
+        assert value, "Must not be empty"
+        return value
+
+
+class PathConvertor(Convertor):
+    regex = ".*"
+
+    def convert(self, value: str) -> typing.Any:
+        return str(value)
+
+    def to_string(self, value: typing.Any) -> str:
+        return str(value)
+
+
+class IntegerConvertor(Convertor):
+    regex = "[0-9]+"
+
+    def convert(self, value: str) -> typing.Any:
+        return int(value)
+
+    def to_string(self, value: typing.Any) -> str:
+        value = int(value)
+        assert value >= 0, "Negative integers are not supported"
+        return str(value)
+
+
+class FloatConvertor(Convertor):
+    regex = "[0-9]+(.[0-9]+)?"
+
+    def convert(self, value: str) -> typing.Any:
+        return float(value)
+
+    def to_string(self, value: typing.Any) -> str:
+        value = float(value)
+        assert value >= 0.0, "Negative floats are not supported"
+        assert not math.isnan(value), "NaN values are not supported"
+        assert not math.isinf(value), "Infinite values are not supported"
+        return ("%0.20f" % value).rstrip("0").rstrip(".")
+
+
+CONVERTOR_TYPES = {
+    "str": StringConvertor(),
+    "path": PathConvertor(),
+    "int": IntegerConvertor(),
+    "float": FloatConvertor(),
+}

commit fc034f64625ab3859fdd417fdf2512a8401565e5

routing: fix KeyError with optional group
14650bf caused a regression when using an optional regexp group
(`(?P<foo>…)?/…`).

starlette/routing.py
@@ -175,6 +175,8 @@ class Route(BaseRoute):
             if match:
                 matched_params = match.groupdict()
                 for key, value in matched_params.items():
+                    if key not in self.param_convertors:
+                        continue
                     matched_params[key] = self.param_convertors[key].convert(value)
                 path_params = dict(scope.get("path_params", {}))
                 path_params.update(matched_params)

tests/test_routing.py
@@ -65,6 +65,13 @@ def path_convertor(request):
     return JSONResponse({"path": path})
 
 
+@app.route(r"/optional(?P<optional>\+[^/]+)?/(?P<suffix>.*)")
+def optional(request):
+    optional = request.path_params["optional"]
+    suffix = request.path_params["suffix"]
+    return JSONResponse({"optional": optional, "suffix": suffix})
+
+
 @app.websocket_route("/ws")
 async def websocket_endpoint(session):
     await session.accept()
@@ -106,6 +113,16 @@ def test_router():
     response = client.get("/static/123")
     assert response.status_code == 200
     assert response.text == "xxxxx"
+
+
+def test_router_optional():
+    response = client.get("/optional/suffix")
+    assert response.status_code == 200
+    assert response.json() == {"optional": None, "suffix": "suffix"}
+
+    response = client.get("/optional+provided/suffix")
+    assert response.status_code == 200
+    assert response.json() == {"optional": "+provided", "suffix": "suffix"}
 
 
 def test_route_converters():