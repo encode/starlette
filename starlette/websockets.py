437da1c0f07714e23cfb612717a38b675de39305
Tom Christie <tom@tomchristie.com>
2019-01-07 14:21:39 +0000

    type of subprotocol is str, not List[str] (#308)
    * type of subprotocol is str, not List[str]
    
    * Force upgrade cached requirements
    
    * Attempt to force clean test re-run
    
    * Always force latest requirements

--- starlette/websockets.py
+++ starlette/websockets.py
@@ -107,7 +107,7 @@ class WebSocket(HTTPConnection):
         else:
             raise RuntimeError('Cannot call "send" once a close message has been sent.')
 
-    async def accept(self, subprotocol: typing.List[str] = None) -> None:
+    async def accept(self, subprotocol: str = None) -> None:
         if self.client_state == WebSocketState.CONNECTING:
             # If we haven't yet seen the 'connect' message, then wait for it first.
             await self.receive()
