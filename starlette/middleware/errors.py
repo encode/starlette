2e64eee13c4eeceba21f915fe16a01680ae7eddf
Florimond Manca <florimond.manca@gmail.com>
2019-01-15 13:36:19 +0100

    merge DebugGenerator into ServerErrorMiddleware (#323)

--- starlette/middleware/errors.py
+++ starlette/middleware/errors.py
@@ -37,36 +37,6 @@ FRAME_TEMPLATE = """
 """
 
 
-class DebugGenerator:
-    def __init__(self, exc: Exception) -> None:
-        self.exc = exc
-        self.traceback_obj = traceback.TracebackException.from_exception(
-            exc, capture_locals=True
-        )
-        self.error = f"{self.traceback_obj.exc_type.__name__}: {self.traceback_obj}"
-
-    @staticmethod
-    def gen_frame_html(frame: traceback.FrameSummary) -> str:
-        values = {
-            "frame_filename": frame.filename,
-            "frame_lineno": frame.lineno,
-            "frame_name": frame.name,
-            "frame_line": frame.line,
-        }
-        return FRAME_TEMPLATE.format(**values)
-
-    def generate_html(self) -> str:
-        ext_html = "".join(
-            [self.gen_frame_html(frame) for frame in self.traceback_obj.stack]
-        )
-        values = {"style": STYLES, "error": self.error, "ext_html": ext_html}
-
-        return TEMPLATE.format(**values)
-
-    def generate_plain_text(self) -> str:
-        return "".join(traceback.format_tb(self.exc.__traceback__))
-
-
 class ServerErrorMiddleware:
     """
     Handles returning 500 responses when a server error occurs.
@@ -127,14 +97,37 @@ class ServerErrorMiddleware:
             # to optionally raise the error within the test case.
             raise exc from None
 
+    def genenrate_frame_html(self, frame: traceback.FrameSummary) -> str:
+        values = {
+            "frame_filename": frame.filename,
+            "frame_lineno": frame.lineno,
+            "frame_name": frame.name,
+            "frame_line": frame.line,
+        }
+        return FRAME_TEMPLATE.format(**values)
+
+    def generate_html(self, exc: Exception) -> str:
+        traceback_obj = traceback.TracebackException.from_exception(
+            exc, capture_locals=True
+        )
+        ext_html = "".join(
+            self.genenrate_frame_html(frame) for frame in traceback_obj.stack
+        )
+        error = f"{traceback_obj.exc_type.__name__}: {traceback_obj}"
+
+        values = {"style": STYLES, "error": error, "ext_html": ext_html}
+        return TEMPLATE.format(**values)
+
+    def generate_plain_text(self, exc: Exception) -> str:
+        return "".join(traceback.format_tb(exc.__traceback__))
+
     def debug_response(self, request: Request, exc: Exception) -> Response:
         accept = request.headers.get("accept", "")
-        debug_gen = DebugGenerator(exc)
 
         if "text/html" in accept:
-            content = debug_gen.generate_html()
+            content = self.generate_html(exc)
             return HTMLResponse(content, status_code=500)
-        content = debug_gen.generate_plain_text()
+        content = self.generate_plain_text(exc)
         return PlainTextResponse(content, status_code=500)
 
     def error_response(self, request: Request, exc: Exception) -> Response:
