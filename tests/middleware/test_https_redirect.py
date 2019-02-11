c74f5e381e82a2d0c0083e07ae4fdc8cb4ac7eda
Tom Christie <tom@tomchristie.com>
2019-02-11 10:16:33 +0000

    Don't drop explict port on HTTPS redirects

--- tests/middleware/test_https_redirect.py
+++ tests/middleware/test_https_redirect.py
@@ -21,3 +21,13 @@ def test_https_redirect_middleware():
     response = client.get("/", allow_redirects=False)
     assert response.status_code == 301
     assert response.headers["location"] == "https://testserver/"
+
+    client = TestClient(app, base_url="http://testserver:80")
+    response = client.get("/", allow_redirects=False)
+    assert response.status_code == 301
+    assert response.headers["location"] == "https://testserver/"
+
+    client = TestClient(app, base_url="http://testserver:123")
+    response = client.get("/", allow_redirects=False)
+    assert response.status_code == 301
+    assert response.headers["location"] == "https://testserver:123/"
