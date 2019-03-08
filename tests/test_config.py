3eb2b13ab0860d1d690914283ea8330aa384bb65
Daniel Hahler <github@thequod.de>
2019-03-08 20:28:08 +0100

    config: cast=bool: handle "0" and "1" as False/True (#429)
    * config: cast=bool: handle "0" and "1" as False/True
    * tests: use monkeypatch.setenv

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,15 +6,14 @@ from starlette.config import Config, Environ, EnvironError
 from starlette.datastructures import URL, Secret
 
 
-def test_config(tmpdir):
+def test_config(tmpdir, monkeypatch):
     path = os.path.join(tmpdir, ".env")
     with open(path, "w") as file:
         file.write("# Do not commit to source control\n")
-        file.write(
-            "DATABASE_URL=postgres://username:password@localhost/database_name\n"
-        )
+        file.write("DATABASE_URL=postgres://user:pass@localhost/dbname\n")
         file.write("REQUEST_HOSTNAME=example.com\n")
         file.write("SECRET_KEY=12345\n")
+        file.write("BOOL_AS_INT=0\n")
         file.write("\n")
         file.write("\n")
 
@@ -25,9 +24,12 @@ def test_config(tmpdir):
     REQUEST_TIMEOUT = config("REQUEST_TIMEOUT", cast=int, default=10)
     REQUEST_HOSTNAME = config("REQUEST_HOSTNAME")
     SECRET_KEY = config("SECRET_KEY", cast=Secret)
+    assert config("BOOL_AS_INT", cast=bool) is False
 
     assert DEBUG is True
-    assert DATABASE_URL.path == "/database_name"
+    assert DATABASE_URL.path == "/dbname"
+    assert DATABASE_URL.password == "pass"
+    assert DATABASE_URL.username == "user"
     assert REQUEST_TIMEOUT == 10
     assert REQUEST_HOSTNAME == "example.com"
     assert repr(SECRET_KEY) == "Secret('**********')"
@@ -42,9 +44,15 @@ def test_config(tmpdir):
     with pytest.raises(ValueError):
         config.get("REQUEST_HOSTNAME", cast=bool)
 
-    os.environ["STARLETTE_EXAMPLE_TEST"] = "123"
     config = Config()
+    monkeypatch.setenv("STARLETTE_EXAMPLE_TEST", "123")
+    monkeypatch.setenv("BOOL_AS_INT", "1")
     assert config.get("STARLETTE_EXAMPLE_TEST", cast=int) == 123
+    assert config.get("BOOL_AS_INT", cast=bool) is True
+
+    monkeypatch.setenv("BOOL_AS_INT", "2")
+    with pytest.raises(ValueError):
+        config.get("BOOL_AS_INT", cast=bool)
 
 
 def test_environ():
