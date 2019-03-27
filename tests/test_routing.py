d3f3a7f71ce733912d33616565581c4e475fc26a
Eugene Molotov <em92@users.noreply.github.com>
2019-03-27 14:37:16 +0500

    Fixed test_combo

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -367,9 +367,9 @@ combo_route_app = Router(
 
 
 def test_combo():
-    assert combo_route_app.url_path_for("combo") == "/combo/"
-    assert combo_route_app.url_path_for("combo", cat="sample") == "/combo/cat/sample"
-    assert combo_route_app.url_path_for("combo", page=1) == "/combo/1"
+    assert combo_route_app.url_path_for("combo", cat=None, page=None) == "/combo/"
+    assert combo_route_app.url_path_for("combo", cat="sample", page=None) == "/combo/cat/sample"
+    assert combo_route_app.url_path_for("combo", cat=None, page=1) == "/combo/1"
     assert (
         combo_route_app.url_path_for("combo", cat="sample", page=1)
         == "/combo/cat/sample/1"
