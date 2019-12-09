63335d8bc819f86e60d0fd7825d685e507cec7e7
sysid <thomas.we.weber@gmx.de>
2019-12-09 20:46:06 +0100

    chore: testing completed

--- starlette/sse.py
+++ starlette/sse.py
@@ -1,12 +1,21 @@
 import asyncio
 import contextlib
 import enum
+import inspect
 import io
+import logging
 import re
-from typing import Any, Optional, Generator
+from datetime import datetime
+from typing import Any, Optional, Generator, Union, Dict, AsyncGenerator
+
+from aiostream import stream
 
 from starlette.background import BackgroundTask
-from starlette.responses import StreamingResponse
+from starlette.concurrency import iterate_in_threadpool
+from starlette.responses import Response
+from starlette.types import Scope, Receive, Send
+
+_log = logging.getLogger(__name__)
 
 
 class SseState(enum.Enum):
@@ -15,16 +24,15 @@ class SseState(enum.Enum):
     CLOSED = 2
 
 
-# Legacy proxy servers are known to, in certain cases, drop HTTP connections after a short timeout.
-# To protect against such proxy servers, authors can include a comment line (one starting with a ':' character) every 15 seconds or so.
 class ServerSentEvent:
     def __init__(
             self,
-            data: str,
+            data: Any,
             *,
             event: Optional[str] = None,
             id: Optional[int] = None,
             retry: Optional[int] = None,
+            sep: str = None
     ) -> None:
         self.data = data
         self.event = event
@@ -33,7 +41,7 @@ class ServerSentEvent:
 
         self.DEFAULT_SEPARATOR = "\r\n"
         self.LINE_SEP_EXPR = re.compile(r"\r\n|\r|\n")
-        self._sep = self.DEFAULT_SEPARATOR
+        self._sep = sep if sep is not None else self.DEFAULT_SEPARATOR
 
     def encode(self) -> bytes:
         """Send data using EventSource protocol
@@ -54,35 +62,53 @@ class ServerSentEvent:
         if self.id is not None:
             buffer.write(self.LINE_SEP_EXPR.sub("", f"id: {self.id}"))
             buffer.write(self._sep)
+
         if self.event is not None:
             buffer.write(self.LINE_SEP_EXPR.sub("", f"event: {self.event}"))
             buffer.write(self._sep)
-        for chunk in self.LINE_SEP_EXPR.split(self.data):
+
+        for chunk in self.LINE_SEP_EXPR.split(str(self.data)):
             buffer.write(f"data: {chunk}")
             buffer.write(self._sep)
+
         if self.retry is not None:
             if not isinstance(self.retry, int):
                 raise TypeError("retry argument must be int")
             buffer.write(f"retry: {self.retry}")
             buffer.write(self._sep)
+
         buffer.write(self._sep)
         return buffer.getvalue().encode("utf-8")
 
 
-class EventSourceResponse(StreamingResponse):
+class EventSourceResponse(Response):
+    """ Implements the ServerSentEvent Protocol: https://www.w3.org/TR/2009/WD-eventsource-20090421/
 
-    DEFAULT_PING_INTERVAL = 15
+        Responses must not be compressed by middleware in order to work properly.
+    """
 
     def __init__(
             self,
-            content: Generator[ServerSentEvent, None, None],
+            content: Union[
+                Generator[Union[str, Dict], None, None],
+                AsyncGenerator[Union[str, Dict], None]
+            ],
             status_code: int = 200,
             headers: dict = None,
+            media_type: str = "text/html",
             background: BackgroundTask = None,
+            ping: float = 15,
             sep: str = None
     ) -> None:
+        self.sep = sep
+        if inspect.isasyncgen(content):
+            self.body_iterator = content
+        else:
+            self.body_iterator = iterate_in_threadpool(content)
+        self.status_code = status_code
+        self.media_type = self.media_type if media_type is None else media_type
+        self.background = background
 
-        # assert scope["type"] == "http"
         _headers = dict()
         if headers is not None:
             _headers.update(headers)
@@ -92,23 +118,50 @@ class EventSourceResponse(StreamingResponse):
         _headers['Cache-Control'] = 'no-cache'
         _headers['Connection'] = 'keep-alive'
         _headers['X-Accel-Buffering'] = 'no'
-        _headers['Transfer-Encoding'] = 'chunked'
+        # _headers['Transfer-Encoding'] = 'chunked'
 
-        super(EventSourceResponse, self).__init__(
-            content=content,
-            status_code=status_code,
-            headers=_headers,
-            media_type="text/html",
-            background=background
-        )
+        self.init_headers(_headers)
 
         self._loop = None
-        self._ping_interval = self.DEFAULT_PING_INTERVAL
+        self._ping_interval = ping
         self._ping_task = None
+        self.active = True
 
         self._loop = asyncio.get_event_loop()
-        self._ping_task = self._loop.create_task(self._ping())
-        _ = None
+        self._ping_task = None
+
+        # self.combined_streams = stream.merge(self.body_iterator, self._ping())
+        self.combined_streams = stream.merge(self.body_iterator)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        await send(
+            {
+                "type": "http.response.start",
+                "status": self.status_code,
+                "headers": self.raw_headers,
+            }
+        )
+
+        self._ping_task = self._loop.create_task(self._ping(send))
+
+        async with self.combined_streams.stream() as streamer:
+            async for data in streamer:
+                if isinstance(data, dict):
+                    chunk = ServerSentEvent(**data, sep=self.sep).encode()
+                else:
+                    chunk = ServerSentEvent(str(data), sep=self.sep).encode()
+                _log.debug(f"chunk: {chunk}")
+                # if not isinstance(chunk, bytes):
+                #     chunk = chunk.encode(self.charset)
+                await send({"type": "http.response.body", "body": chunk, "more_body": True})
+
+        self.stop_streaming()
+        await self.wait()
+        _log.debug(f"streaming stopped.")
+        await send({"type": "http.response.body", "body": b"", "more_body": False})
+
+        if self.background is not None:
+            await self.background()
 
     async def wait(self):
         """EventSourceResponse object is used for streaming data to the client,
@@ -119,6 +172,7 @@ class EventSourceResponse(StreamingResponse):
             raise RuntimeError('Response is not started')
         with contextlib.suppress(asyncio.CancelledError):
             await self._ping_task
+            _log.debug(f"SSE ping stopped.")
 
     def stop_streaming(self):
         """Used in conjunction with ``wait`` could be called from other task
@@ -143,20 +197,22 @@ class EventSourceResponse(StreamingResponse):
         :param int value: interval in sec between two ping values.
         """
 
-        if not isinstance(value, int):
-            raise TypeError("ping interval must be int")
+        if not isinstance(value, float):
+            raise TypeError("ping interval must be float")
         if value < 0:
             raise ValueError("ping interval must be greater then 0")
 
         self._ping_interval = value
 
-    async def _ping(self):
-        # periodically send ping to the browser. Any message that
-        # starts with ":" colon ignored by a browser and could be used
-        # as ping message.
-        while True:
+    async def _ping(self, send: Send):
+        # Legacy proxy servers are known to, in certain cases, drop HTTP connections after a short timeout.
+        # To protect against such proxy servers, authors can include a comment line
+        # (one starting with a ':' character) every 15 seconds or so.
+        while self.active:
             await asyncio.sleep(self._ping_interval, loop=self._loop)
-            await self.write(': ping{0}{0}'.format(self._sep).encode('utf-8'))  # TODO: Fix
+            ping = ServerSentEvent(f": ping {datetime.utcnow()}").encode()
+            _log.debug(f"ping: {ping}")
+            await send({"type": "http.response.body", "body": ping, "more_body": True})
 
     async def __aenter__(self):
         return self
--- tests/test_sse.py
+++ tests/test_sse.py
@@ -1,19 +1,105 @@
+import asyncio
+import logging
+
+import pytest
+from aiostream import stream
+
 from starlette.responses import StreamingResponse
 from starlette.sse import EventSourceResponse, ServerSentEvent
 from starlette.testclient import TestClient
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-def test_sync_streaming_response():
+
+@pytest.mark.parametrize('input,expected', [
+    ('integer', b"data: 1\r\n\r\n"),
+    ('dict1', b"data: 1\r\n\r\n"),
+    ('dict2', b'event: message\r\ndata: 1\r\n\r\n'),
+])
+def test_sync_event_source_response(input, expected):
     async def app(scope, receive, send):
-        def numbers(minimum, maximum):
+        async def numbers(minimum, maximum):
             for i in range(minimum, maximum + 1):
-                yield ServerSentEvent(str(i)).encode()
+                await asyncio.sleep(0.1)
+                if input == 'integer':
+                    yield i
+                elif input == 'dict1':
+                    yield dict(data=i)
+                elif input == 'dict2':
+                    yield dict(data=i, event="message")
 
         generator = numbers(1, 5)
-        response = EventSourceResponse(generator)
+        response = EventSourceResponse(generator, ping=0.2)
         await response(scope, receive, send)
 
     client = TestClient(app)
     response = client.get("/")
-    assert response.content == b'data: 1\r\n\r\ndata: 2\r\n\r\ndata: 3\r\n\r\ndata: 4\r\n\r\ndata: 5\r\n\r\n'
+    assert response.content.decode().count('ping') == 2
+    assert expected in response.content
+    print(response.content)
 
+
+@pytest.mark.asyncio
+async def test_wait_stop_streaming_errors():
+    response = EventSourceResponse(0)
+    with pytest.raises(RuntimeError) as ctx:
+        await response.wait()
+    assert str(ctx.value) == 'Response is not started'
+
+    with pytest.raises(RuntimeError) as ctx:
+        response.stop_streaming()
+    assert str(ctx.value) == 'Response is not started'
+
+
+def test_compression_not_implemented():
+    response = EventSourceResponse(0)
+    with pytest.raises(NotImplementedError):
+        response.enable_compression()
+
+
+@pytest.mark.parametrize('input, expected', [
+    ("foo", b'data: foo\r\n\r\n'),
+    (dict(data="foo", event="bar"), b'event: bar\r\ndata: foo\r\n\r\n'),
+    (dict(data="foo", event="bar", id="xyz"), b'id: xyz\r\nevent: bar\r\ndata: foo\r\n\r\n'),
+    (dict(data="foo", event="bar", id="xyz", retry=1), b'id: xyz\r\nevent: bar\r\ndata: foo\r\nretry: 1\r\n\r\n'),
+])
+def test_server_sent_event(input, expected):
+    print(input, expected)
+    if isinstance(input, str):
+        assert ServerSentEvent(input).encode() == expected
+    else:
+        assert ServerSentEvent(**input).encode() == expected
+
+
+@pytest.mark.parametrize('stream_sep,line_sep',
+                         [('\n', '\n',),
+                          ('\n', '\r',),
+                          ('\n', '\r\n',),
+                          ('\r', '\n',),
+                          ('\r', '\r',),
+                          ('\r', '\r\n',),
+                          ('\r\n', '\n',),
+                          ('\r\n', '\r',),
+                          ('\r\n', '\r\n',), ],
+                         ids=('stream-LF:line-LF',
+                              'stream-LF:line-CR',
+                              'stream-LF:line-CR+LF',
+                              'stream-CR:line-LF',
+                              'stream-CR:line-CR',
+                              'stream-CR:line-CR+LF',
+                              'stream-CR+LF:line-LF',
+                              'stream-CR+LF:line-CR',
+                              'stream-CR+LF:line-CR+LF',
+                              ))
+def test_multiline_data(stream_sep, line_sep):
+    lines = line_sep.join(['foo', 'bar', 'xyz'])
+    result = ServerSentEvent(lines, event="event", sep=stream_sep).encode()
+    assert result == "event: event{0}data: foo{0}data: bar{0}data: xyz{0}{0}".format(stream_sep).encode()
+
+
+@pytest.mark.parametrize('sep',
+                         ['\n', '\r', '\r\n'],
+                         ids=('LF', 'CR', 'CR+LF'))
+def test_custom_sep(sep):
+    result = ServerSentEvent('foo', event="event", sep=sep).encode()
+    assert result == "event: event{0}data: foo{0}{0}".format(sep).encode()
