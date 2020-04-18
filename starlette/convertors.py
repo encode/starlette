c22699a4abe0f4330c4651d7213c4aebaf08e950
Jamie Hewland <jhewland@gmail.com>
2020-04-18 10:37:15 +0200

    Genericise converter return types

--- starlette/convertors.py
+++ starlette/convertors.py
@@ -1,59 +1,61 @@
 import math
-import typing
+from typing import Any, Dict, Generic, TypeVar
+
+T = TypeVar("T")
 
 
-class Convertor:
+class Convertor(Generic[T]):
     regex = ""
 
-    def convert(self, value: str) -> typing.Any:
+    def convert(self, value: str) -> T:
         raise NotImplementedError()  # pragma: no cover
 
-    def to_string(self, value: typing.Any) -> str:
+    def to_string(self, value: Any) -> str:
         raise NotImplementedError()  # pragma: no cover
 
 
-class StringConvertor(Convertor):
+class StringConvertor(Convertor[str]):
     regex = "[^/]+"
 
-    def convert(self, value: str) -> typing.Any:
+    def convert(self, value: str) -> str:
         return value
 
-    def to_string(self, value: typing.Any) -> str:
+    def to_string(self, value: Any) -> str:
         value = str(value)
         assert "/" not in value, "May not contain path separators"
         assert value, "Must not be empty"
         return value
 
 
-class PathConvertor(Convertor):
+class PathConvertor(Convertor[str]):
     regex = ".*"
 
-    def convert(self, value: str) -> typing.Any:
+    def convert(self, value: str) -> str:
         return str(value)
 
-    def to_string(self, value: typing.Any) -> str:
+    def to_string(self, value: Any) -> str:
         return str(value)
 
 
-class IntegerConvertor(Convertor):
+class IntegerConvertor(Convertor[int]):
     regex = "[0-9]+"
 
-    def convert(self, value: str) -> typing.Any:
+    def convert(self, value: str) -> int:
         return int(value)
 
-    def to_string(self, value: typing.Any) -> str:
+    def to_string(self, value: Any) -> str:
         value = int(value)
         assert value >= 0, "Negative integers are not supported"
         return str(value)
 
 
-class FloatConvertor(Convertor):
+class FloatConvertor(Convertor[float]):
     regex = "[0-9]+(.[0-9]+)?"
 
-    def convert(self, value: str) -> typing.Any:
+    def convert(self, value: str) -> float:
         return float(value)
 
-    def to_string(self, value: typing.Any) -> str:
+    def to_string(self, value: Any) -> str:
         value = float(value)
         assert value >= 0.0, "Negative floats are not supported"
         assert not math.isnan(value), "NaN values are not supported"
@@ -61,7 +63,7 @@ class FloatConvertor(Convertor):
         return ("%0.20f" % value).rstrip("0").rstrip(".")
 
 
-CONVERTOR_TYPES = {
+CONVERTOR_TYPES: Dict[str, Convertor] = {
     "str": StringConvertor(),
     "path": PathConvertor(),
     "int": IntegerConvertor(),
