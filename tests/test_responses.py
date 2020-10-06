ac6493218d82d4862a4125a2fd24ca7317db473c
witling <noreply@my.email>
2020-10-06 09:58:38 +0200

    comply with pep 492

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -297,7 +297,7 @@ def test_streaming_response_custom_iterator():
 def test_streaming_response_custom_iterable():
     async def app(scope, receive, send):
         class CustomAsyncIterable:
-            def __aiter__(self):
+            async def __aiter__(self):
                 for i in range(5):
                     yield str(i + 1)
 
