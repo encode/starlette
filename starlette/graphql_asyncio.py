564782536a466ecd36f98a86d077847bcee35a8e
NatachaMeow <64448602+NatachaMeow@users.noreply.github.com>
2020-11-14 05:56:57 -0500

    tox linting.

--- starlette/graphql_asyncio.py
+++ starlette/graphql_asyncio.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
-from typing import Any
 
 from asyncio import Future, get_event_loop, iscoroutine, wait
+from typing import Any
 
 from promise import Promise
 
