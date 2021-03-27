9f76d4293df380e842090941557999fb344dbbec
Jordan Speicher <jordan@jspeicher.com>
2021-03-27 18:54:59 -0500

    Formatting fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 
-@pytest.fixture(params=[
-    pytest.param(('asyncio', {'use_uvloop': True}), id='asyncio+uvloop'),
-    pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
-    pytest.param(('trio', {'restrict_keyboard_interrupt_to_checkpoints': True}), id='trio')
-], autouse=True)
+
+@pytest.fixture(
+    params=[
+        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
+        pytest.param(("asyncio", {"use_uvloop": False}), id="asyncio"),
+        pytest.param(
+            ("trio", {"restrict_keyboard_interrupt_to_checkpoints": True}), id="trio"
+        ),
+    ],
+    autouse=True,
+)
 def anyio_backend(request):
     return request.param
