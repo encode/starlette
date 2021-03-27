01dd81370a7bcd76ebd5a8146ca27988c5643aca
Jordan Speicher <jordan@jspeicher.com>
2021-03-27 18:53:09 -0500

    Test on asyncio, asyncio+uvloop, and trio

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture(params=[
+    pytest.param(('asyncio', {'use_uvloop': True}), id='asyncio+uvloop'),
+    pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
+    pytest.param(('trio', {'restrict_keyboard_interrupt_to_checkpoints': True}), id='trio')
+], autouse=True)
+def anyio_backend(request):
+    return request.param
