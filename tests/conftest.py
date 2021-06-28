731d2bcc3574ec6a963eb8541fcad9b73dde771f
Thomas Grainger <tagrain@gmail.com>
2021-06-28 21:18:30 +0100

    document where anyio_backend_name comes from

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,8 @@ def no_trio_support(anyio_backend_name):
 
 @pytest.fixture
 def test_client_factory(anyio_backend_name, anyio_backend_options):
+    # anyio_backend_name defined by:
+    # https://anyio.readthedocs.io/en/stable/testing.html#specifying-the-backends-to-run-on
     return functools.partial(
         TestClient,
         backend=anyio_backend_name,
