7ee807fca971baee88cfb567e22ae86066e6e97a
lishukan <lishukan@qq.com>
2021-07-26 17:03:56 +0800

    Add a encoder argument for the function __init__ of class - JsonResponse (#1250) This argument have default value :None. And this argument is  related to argument 'cls' of function - json.dumps(...cls=)

--- starlette/responses.py
+++ starlette/responses.py
@@ -152,6 +152,18 @@ class PlainTextResponse(Response):
 
 class JSONResponse(Response):
     media_type = "application/json"
+    def __init__(
+        self,
+        content: typing.Any = None,
+        status_code: int = 200,
+        headers: dict = None,
+        media_type: str = None,
+        background: BackgroundTask = None, 
+        encoder:typing.Any =None,
+    ) -> None:
+        self.encoder = encoder
+        super().__init__(content,status_code,headers,media_type,background)
+    
 
     def render(self, content: typing.Any) -> bytes:
         return json.dumps(
@@ -160,6 +172,7 @@ class JSONResponse(Response):
             allow_nan=False,
             indent=None,
             separators=(",", ":"),
+            cls=self.encoder,
         ).encode("utf-8")
 
 
