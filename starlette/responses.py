d6c2eef1ba1ae1b5a9e1f0a82e0d77f68adbfff1
Nebulizer1213 <fixingg@gmail.com>
2021-08-22 05:31:50 -0400

    Update responses.py

--- starlette/responses.py
+++ starlette/responses.py
@@ -153,7 +153,14 @@ class PlainTextResponse(Response):
 class JSONResponse(Response):
     media_type = "application/json"
 
-    def __init__(self, content: typing.Any = None, status_code: int = 200, headers: dict = None, media_type: str = None, background: BackgroundTask = None, indent: int = None):
+    def __init__(
+        self, 
+        content: typing.Any = None, 
+        status_code: int = 200, 
+        headers: dict = None, 
+        media_type: str = None, 
+        background: BackgroundTask = None, 
+        indent: int = None) -> None:
         self.indent = indent
         super().__init__(content=content, status_code=status_code, headers=headers, media_type=media_type, background=background)
 
