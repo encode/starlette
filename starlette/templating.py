9228c2d12c29c7e6a43d614c0190f318863712d8
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-09-26 13:39:29 +1000

    Update the Jinja2Templates() to allow PathLike

--- starlette/templating.py
+++ starlette/templating.py
@@ -1,3 +1,4 @@
+from os import PathLike
 import typing
 
 from starlette.background import BackgroundTask
@@ -54,11 +55,11 @@ class Jinja2Templates:
     return templates.TemplateResponse("index.html", {"request": request})
     """
 
-    def __init__(self, directory: str) -> None:
+    def __init__(self, directory: PathLike[str]) -> None:
         assert jinja2 is not None, "jinja2 must be installed to use Jinja2Templates"
         self.env = self._create_env(directory)
 
-    def _create_env(self, directory: str) -> "jinja2.Environment":
+    def _create_env(self, directory: PathLike[str]) -> "jinja2.Environment":
         @pass_context
         def url_for(context: dict, name: str, **path_params: typing.Any) -> str:
             request = context["request"]
