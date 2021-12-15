04a69953c9afe3e80d77507001f1bb02dc942118
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-15 09:50:15 -0600

    fix: pass usedforsecurity to hashlib.md5 to fix error on FIPS-enabled systems

---
+++ starlette/_crypto.py
@@ -0,0 +1,14 @@
+import hashlib
+import inspect
+
+
+# TODO: Remove when dropping support for PY38. inspect.signature() is used to
+# detect whether the usedforsecurity argument is available as this fix may also
+# have been applied by downstream package maintainers to other versions in
+# their repositories.
+if "usedforsecurity" in inspect.signature(hashlib.md5).parameters:
+    def md5_hexdigest(data: bytes, *, usedforsecurity: bool = True) -> str:
+        return hashlib.md5(data, usedforsecurity=usedforsecurity).hexdigest()
+else:
+    def md5_hexdigest(data: bytes, *, usedforsecurity: bool = True) -> str:
+        return hashlib.md5(data).hexdigest()
