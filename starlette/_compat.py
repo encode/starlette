fbf37452308ce9a4ffea7dbdbee4a4f238a23d81
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-15 11:32:19 -0600

    work around openssl lacking signature

--- starlette/_compat.py
+++ starlette/_compat.py
@@ -1,11 +1,12 @@
 import hashlib
-import inspect
 
-# TODO: Remove when dropping support for PY38. inspect.signature() is used to
+# TODO: Remove when dropping support for PY38. a try/except is used to
 # detect whether the usedforsecurity argument is available as this fix may also
 # have been applied by downstream package maintainers to other versions in
 # their repositories.
-if "usedforsecurity" in inspect.signature(hashlib.md5).parameters:
+try:
+
+    hashlib.md5(b"data", usedforsecurity=True)  # type: ignore[call-arg]
 
     def md5_hexdigest(data: bytes, *, usedforsecurity: bool = True) -> str:
         return hashlib.md5(  # type: ignore[call-arg]
@@ -13,7 +14,7 @@ if "usedforsecurity" in inspect.signature(hashlib.md5).parameters:
         ).hexdigest()
 
 
-else:
+except TypeError:
 
     def md5_hexdigest(data: bytes, *, usedforsecurity: bool = True) -> str:
         return hashlib.md5(data).hexdigest()
