1fd2fccdff33e23314262d91ba2ca3628a163260
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-15 11:36:52 -0600

    add pragma: no cover

--- starlette/_compat.py
+++ starlette/_compat.py
@@ -14,7 +14,7 @@ try:
         ).hexdigest()
 
 
-except TypeError:
+except TypeError:  # pragma: no cover
 
     def md5_hexdigest(data: bytes, *, usedforsecurity: bool = True) -> str:
         return hashlib.md5(data).hexdigest()
