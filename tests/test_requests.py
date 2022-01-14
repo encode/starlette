3bf4d153b75eb0d74f937830362a6eb1c3c2504d
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-14 10:58:26 -0800

    close form

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -127,6 +127,7 @@ def test_request_multipart_file_max_size(test_client_factory):
         assert isinstance(uploadfile, UploadFile)
         file = uploadfile.file
         response = JSONResponse({"rolled": getattr(file, "_rolled", True)})
+        await form.close()
         await response(scope, receive, send)
 
     client = test_client_factory(app)
