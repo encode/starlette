3c76feacb8ef25c77036d3c4f6667a3c22639d27
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-27 13:55:23 +0100

    Simplify tests and refresh CONTENT_TYPES

--- tests/test_convertors.py
+++ tests/test_convertors.py
@@ -1,8 +1,18 @@
 import datetime as dt
 
+import pytest
+
+from starlette import convertors
 from starlette.convertors import Convertor, register_url_convertor
 from starlette.responses import JSONResponse
-from starlette.routing import Router
+from starlette.routing import Route, Router
+
+
+@pytest.fixture(scope="module", autouse=True)
+def refresh_convertor_types():
+    convert_types = convertors.CONVERTOR_TYPES.copy()
+    yield
+    convertors.CONVERTOR_TYPES = convert_types
 
 
 class DateTimeConvertor(Convertor):
@@ -15,59 +25,27 @@ class DateTimeConvertor(Convertor):
         return value.strftime("%Y-%m-%dT%H:%M:%S")
 
 
-register_url_convertor("datetime", DateTimeConvertor())
+@pytest.fixture(scope="function")
+def app() -> Router:
+    register_url_convertor("datetime", DateTimeConvertor())
+
+    def datetime_convertor(request):
+        param = request.path_params["param"]
+        assert isinstance(param, dt.datetime)
+        return JSONResponse({"datetime": param.strftime("%Y-%m-%dT%H:%M:%S")})
+
+    return Router(
+        routes=[
+            Route(
+                "/datetime/{param:datetime}",
+                endpoint=datetime_convertor,
+                name="datetime-convertor",
+            )
+        ]
+    )
 
 
-class DateConvertor(Convertor):
-    regex = "[0-9]{4}-[0-9]{2}-[0-9]{2}"
-
-    def convert(self, value: str) -> dt.date:
-        return dt.datetime.strptime(value, "%Y-%m-%d").date()
-
-    def to_string(self, value: dt.datetime) -> str:
-        return value.strftime("%Y-%m-%d")
-
-
-register_url_convertor("date", DateConvertor())
-
-
-class TimeConvertor(Convertor):
-    regex = "[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]+)?"
-
-    def convert(self, value: str) -> dt.time:
-        return dt.datetime.strptime(value, "%H:%M:%S").time()
-
-    def to_string(self, value: dt.datetime) -> str:
-        return value.strftime("%H:%M:%S")
-
-
-register_url_convertor("time", TimeConvertor())
-
-app = Router()
-
-
-@app.route("/datetime/{param:datetime}", name="datetime-convertor")
-def datetime_convertor(request):
-    param = request.path_params["param"]
-    assert isinstance(param, dt.datetime)
-    return JSONResponse({"datetime": param.strftime("%Y-%m-%dT%H:%M:%S")})
-
-
-@app.route("/date/{param:date}", name="date-convertor")
-def date_convertor(request):
-    param = request.path_params["param"]
-    assert isinstance(param, dt.date)
-    return JSONResponse({"date": param.strftime("%Y-%m-%d")})
-
-
-@app.route("/time/{param:time}", name="time-convertor")
-def time_convertor(request):
-    param = request.path_params["param"]
-    assert isinstance(param, dt.time)
-    return JSONResponse({"time": param.strftime("%H:%M:%S")})
-
-
-def test_datetime_convertor(test_client_factory):
+def test_datetime_convertor(test_client_factory, app: Router):
     client = test_client_factory(app)
     response = client.get("/datetime/2020-01-01T00:00:00")
     assert response.json() == {"datetime": "2020-01-01T00:00:00"}
@@ -75,25 +53,4 @@ def test_datetime_convertor(test_client_factory):
     assert (
         app.url_path_for("datetime-convertor", param=dt.datetime(1996, 1, 22, 23, 0, 0))
         == "/datetime/1996-01-22T23:00:00"
-    )
-
-
-def test_date_convertor(test_client_factory):
-    client = test_client_factory(app)
-    response = client.get("/date/2020-01-01")
-    assert response.json() == {"date": "2020-01-01"}
-
-    assert (
-        app.url_path_for("date-convertor", param=dt.date(1996, 1, 22))
-        == "/date/1996-01-22"
-    )
-
-
-def test_time_convertor(test_client_factory):
-    client = test_client_factory(app)
-    response = client.get("/time/00:00:00")
-    assert response.json() == {"time": "00:00:00"}
-
-    assert (
-        app.url_path_for("time-convertor", param=dt.time(23, 0, 0)) == "/time/23:00:00"
     )
