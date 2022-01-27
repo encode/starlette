172a47978a68fc2d7865f59dc5c0d98efcddde84
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-27 14:17:14 +0100

    Simplify datetime import

--- docs/routing.md
+++ docs/routing.md
@@ -59,16 +59,18 @@ If you need a different converter that is not defined, you can create your own.
 See below an example on how to create a `datetime` convertor, and how to register it:
 
 ```python
+from datetime import datetime
+
 from starlette.convertors import Convertor, register_url_convertor
 
 
 class DateTimeConvertor(Convertor):
     regex = "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]+)?"
 
-    def convert(self, value: str) -> dt.datetime:
+    def convert(self, value: str) -> datetime:
         return dt.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
 
-    def to_string(self, value: dt.datetime) -> str:
+    def to_string(self, value: datetime) -> str:
         return value.strftime("%Y-%m-%dT%H:%M:%S")
 
 register_url_convertor("datetime", DateTimeConvertor())
--- tests/test_convertors.py
+++ tests/test_convertors.py
@@ -1,4 +1,4 @@
-import datetime as dt
+from datetime import datetime
 
 import pytest
 
@@ -18,8 +18,8 @@ def refresh_convertor_types():
 class DateTimeConvertor(Convertor):
     regex = "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]+)?"
 
-    def convert(self, value: str) -> dt.datetime:
-        return dt.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
+    def convert(self, value: str) -> datetime:
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
 
     def to_string(self, value: dt.datetime) -> str:
         return value.strftime("%Y-%m-%dT%H:%M:%S")
@@ -31,7 +31,7 @@ def app() -> Router:
 
     def datetime_convertor(request):
         param = request.path_params["param"]
-        assert isinstance(param, dt.datetime)
+        assert isinstance(param, datetime)
         return JSONResponse({"datetime": param.strftime("%Y-%m-%dT%H:%M:%S")})
 
     return Router(
@@ -51,6 +51,6 @@ def test_datetime_convertor(test_client_factory, app: Router):
     assert response.json() == {"datetime": "2020-01-01T00:00:00"}
 
     assert (
-        app.url_path_for("datetime-convertor", param=dt.datetime(1996, 1, 22, 23, 0, 0))
+        app.url_path_for("datetime-convertor", param=datetime(1996, 1, 22, 23, 0, 0))
         == "/datetime/1996-01-22T23:00:00"
     )
