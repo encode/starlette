417a3d769e83077e289118533e2250d9a812e7cb
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-29 13:30:14 -0600

    remove unused type: ignore

--- starlette/requests.py
+++ starlette/requests.py
@@ -136,7 +136,7 @@ class HTTPConnection(Mapping):
     @property
     def client(self) -> Address:
         host, port = self.scope.get("client") or (None, None)
-        return Address(host=host, port=port)  # type: ignore[arg-type]
+        return Address(host=host, port=port)
 
     @property
     def session(self) -> dict:
