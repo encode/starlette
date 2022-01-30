f0a68d7e113df398c5ffe9d65d7a1025e9711599
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-30 05:59:06 -0600

    make mypy happy

--- starlette/testclient.py
+++ starlette/testclient.py
@@ -87,7 +87,7 @@ def _is_asgi3(app: typing.Union[ASGI2App, ASGI3App]) -> bool:
         return hasattr(app, "__await__")
     elif inspect.isfunction(app):
         return asyncio.iscoroutinefunction(app)
-    call = getattr(app, "__call__", None)
+    call: typing.Any = getattr(app, "__call__", None)
     return asyncio.iscoroutinefunction(call) or get_origin(
         get_type_hints(call).get("return")
     ) in (collections.Awaitable, typing.Awaitable)
