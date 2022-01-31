565898b3fdce24f85ec279fe998832da1a0ee664
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-02-01 00:18:13 +0100

    Remove manual contextvar copy logic (#1421)
    * Remove manual contextvar copy logic
    
    * Add test
    
    Co-authored-by: Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     package_data={"starlette": ["py.typed"]},
     include_package_data=True,
     install_requires=[
-        "anyio>=3.0.0,<4",
+        "anyio>=3.4.0,<4",
         "typing_extensions; python_version < '3.10'",
         "contextlib2 >= 21.6.0; python_version < '3.7'",
     ],
