54bc77bce298fad61d6abe6f3036ba7781b95a06
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-03-27 19:34:12 -0500

    Update templating.py

--- starlette/templating.py
+++ starlette/templating.py
@@ -8,12 +8,13 @@ from starlette.types import Receive, Scope, Send
 try:
     import jinja2
 
-    # @contextfunction renamed to @pass_context in Jinja 3.0, to be removed in 3.1
+    # @contextfunction renamed to @pass_context in Jinja 3.0, and was removed in 3.1
+    # hence we try to get pass_context (most installs will be >=3.1)
+    # and fall back to contextfunction
+    # (adding a type ignore for mypy to let us access an attribute that may not exist)
     if hasattr(jinja2, "pass_context"):
         pass_context = jinja2.pass_context
     else:  # pragma: nocover
-        # mypy will complain about a missing attribute if Jinja >= 3.1 is installed
-        # but that will never happen because the branch above will be true
         pass_context = jinja2.contextfunction  # type: ignore[attr-defined]
 except ImportError:  # pragma: nocover
     jinja2 = None  # type: ignore
