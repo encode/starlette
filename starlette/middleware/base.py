2e8ab8f752d64bb0ec485cd9bc8708f1df26d103
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-04-29 21:52:23 +0200

    Do not send empty data to `StreamingResponse` on `BaseHTTPMiddleware`

--- starlette/middleware/base.py
+++ starlette/middleware/base.py
@@ -52,7 +52,11 @@ class BaseHTTPMiddleware:
                 async with recv_stream:
                     async for message in recv_stream:
                         assert message["type"] == "http.response.body"
-                        yield message.get("body", b"")
+                        body = message.get("body", b"")
+                        if body:
+                            yield body
+                        if not message.get("more_body", False):
+                            break
 
                 if app_exc is not None:
                     raise app_exc
