072e9499e0182fb32e5becfcae95019dd83b4f33
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-07-02 10:53:52 -0500

    remove import

--- tests/middleware/test_base.py
+++ tests/middleware/test_base.py
@@ -13,7 +13,6 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse, Response, StreamingResponse
 from starlette.routing import Route, WebSocketRoute
-from starlette.testclient import TestClient
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
 
 
