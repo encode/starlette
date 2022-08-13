ca6b2e03df4bce19b6c0f9fe778f2d404898f714
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-08-13 15:47:33 +0200

    Remove unused imports

--- starlette/middleware/errors.py
+++ starlette/middleware/errors.py
@@ -3,13 +3,11 @@ import inspect
 import traceback
 import typing
 
-from starlette import status
 from starlette._utils import is_async_callable
 from starlette.concurrency import run_in_threadpool
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, PlainTextResponse, Response
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
-from starlette.websockets import WebSocket
 
 STYLES = """
 p {
