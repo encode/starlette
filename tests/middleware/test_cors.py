17fa884f073b4f588844af24ba44e175f198873b
Sanggyu Lee <sanggyu@friendli.ai>
2022-08-29 18:04:47 +0900

    Add Authorization credential test

--- tests/middleware/test_cors.py
+++ tests/middleware/test_cors.py
@@ -57,6 +57,15 @@ def test_cors_allow_all(test_client_factory):
     assert response.headers["access-control-expose-headers"] == "X-Status"
     assert response.headers["access-control-allow-credentials"] == "true"
 
+    # Test Authorization header credentialed response
+    headers = {"Origin": "https://example.org", "Authorization": "Bearer some_token"}
+    response = client.get("/", headers=headers)
+    assert response.status_code == 200
+    assert response.text == "Homepage"
+    assert response.headers["access-control-allow-origin"] == "https://example.org"
+    assert response.headers["access-control-expose-headers"] == "X-Status"
+    assert response.headers["access-control-allow-credentials"] == "true"
+
     # Test non-CORS response
     response = client.get("/")
     assert response.status_code == 200
