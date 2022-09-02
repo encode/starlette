37856a0cc3271ffbc99069ceaae394eb4805b211
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-09-02 07:55:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ sys.stderr.write(
 ===============================
 Unsupported installation method
 ===============================
-httpx no longer supports installation with `python setup.py install`.
+uvicorn no longer supports installation with `python setup.py install`.
 Please use `python -m pip install .` instead.
 """
 )
