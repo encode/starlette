93ec37fe66dbfae5fe6cbadbaeed0cefd456bf24
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-09-06 07:21:55 -0500

    fix linting

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -857,7 +857,9 @@ def test_mount_routes_with_middleware_url_path_for() -> None:
 
 
 def test_mount_asgi_app_with_middleware_url_path_for() -> None:
-    """Mounted ASGI apps do not work with url path for, middleware does not change this"""
+    """Mounted ASGI apps do not work with url path for,
+    middleware does not change this
+    """
     with pytest.raises(NoMatchFound):
         mounted_app_with_middleware.url_path_for("route")
 
