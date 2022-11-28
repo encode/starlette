21672e5305a3c3f41c81dc9a807f9ce7936e03d3
Roy Pasternak <roy@larium.co>
2022-11-28 19:03:05 +0200

    add user given headers support in TestClient.init()

--- starlette/testclient.py
+++ starlette/testclient.py
@@ -368,6 +368,7 @@ class TestClient(httpx.Client):
         backend: str = "asyncio",
         backend_options: typing.Optional[typing.Dict[str, typing.Any]] = None,
         cookies: httpx._client.CookieTypes = None,
+        headers: typing.Dict[str, str] = None,
     ) -> None:
         self.async_backend = _AsyncBackend(
             backend=backend, backend_options=backend_options or {}
@@ -385,10 +386,15 @@ class TestClient(httpx.Client):
             raise_server_exceptions=raise_server_exceptions,
             root_path=root_path,
         )
+        if headers is None:
+            headers = {}
         super().__init__(
             app=self.app,
             base_url=base_url,
-            headers={"user-agent": "testclient"},
+            headers=headers
+            | {
+                "user-agent": "testclient"
+            },  # override user-agent value with "testclient" for backward compatibility
             transport=transport,
             follow_redirects=True,
             cookies=cookies,
