d97b3be38be91bc45bbf7dc1d8d0e3a13d463ac6
Alex Oleshkevich <alex.oleshkevich@gmail.com>
2022-12-27 14:33:28 +0300

    Update tests/test_templates.py
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -34,7 +34,7 @@ def test_template_response_requires_request(tmpdir):
         templates.TemplateResponse("", {})
 
 
-def test_calls_context_processors(tmp_path, test_client_factory, tmpdir):
+def test_calls_context_processors(tmp_path, test_client_factory):
     path = tmp_path / "index.html"
     path.write_text("<html>Hello {{ username }}</html>")
 
