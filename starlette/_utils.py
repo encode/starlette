bf3d7f056678cfb8bbd859fc80f67db997aed6fa
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-02-05 21:56:18 -0800

    trailing comma

--- starlette/_utils.py
+++ starlette/_utils.py
@@ -27,7 +27,7 @@ T_co = typing.TypeVar("T_co", covariant=True)
 # class AwaitableOrContextManager(
 #     typing.Awaitable[T_co],
 #     typing.AsyncContextManager[T_co],
-#     typing.Protocol[T_co]
+#     typing.Protocol[T_co],
 # ):
 #     pass
 class AwaitableOrContextManager(Protocol[T_co]):
