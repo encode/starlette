cc20c8606ede3123a6ab67bf28b8522b1e8aac52
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-03-09 23:30:28 +0100

    Deprecate `on_startup` and `on_shutdown` events (#2070)
    * Revert "Support lifespan state (#2060)"
    
    This reverts commit da6461b239cde16ee9709b7d266c2529c26239d7.
    
    * new implementation
    
    * Deprecate `on_startup` and `on_shutdown` events
    
    * Rename `events.md` by `lifespan.md`
    
    ---------
    
    Co-authored-by: Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>

--- starlette/applications.py
+++ starlette/applications.py
@@ -37,6 +37,9 @@ class Starlette:
     * **on_shutdown** - A list of callables to run on application shutdown.
     Shutdown handler callables do not take any arguments, and may be be either
     standard functions, or async functions.
+    * **lifespan** - A lifespan context function, which can be used to perform
+    startup and shutdown tasks. This is a newer style that replaces the
+    `on_startup` and `on_shutdown` handlers. Use one or the other, not both.
     """
 
     def __init__(
