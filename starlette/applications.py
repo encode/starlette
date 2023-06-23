64d613ab9b42891345c11f2dfe94457797fdda12
Stratos Gerakakis <stratos@gerakakis.net>
2023-06-23 19:38:11 +0200

    Clarify lifespan vs on_startuo/on_shutdown priority
    As per discussion (https://github.com/encode/starlette/discussions/2192), I am trying to make it more clear that if both lifespan AND on_startup/on_shutdown are used, regardless of the subtle hint that both should NOT be used, then only lifespan would be used and  on_startup/on_shutdown would silently be ignored.

--- starlette/applications.py
+++ starlette/applications.py
@@ -41,7 +41,9 @@ class Starlette:
     standard functions, or async functions.
     * **lifespan** - A lifespan context function, which can be used to perform
     startup and shutdown tasks. This is a newer style that replaces the
-    `on_startup` and `on_shutdown` handlers. Use one or the other, not both.
+    `on_startup` and `on_shutdown` handlers. Use one or the other, not both. If
+    both lifespan and on_startup/on_shutdown are used, only lifespan would be
+    used.
     """
 
     def __init__(
