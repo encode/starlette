8243a4f0b33d72f5aa8ba50c85a737427609029a
Viicos <65306057+Viicos@users.noreply.github.com>
2023-06-23 22:44:28 +0200

    Typing: Fix issue with Python 3.7

--- starlette/_compat.py
+++ starlette/_compat.py
@@ -18,7 +18,7 @@ try:
     def md5_hexdigest(
         data: bytes, *, usedforsecurity: bool = True
     ) -> str:  # pragma: no cover
-        return hashlib.md5(data, usedforsecurity=usedforsecurity).hexdigest()
+        return hashlib.md5(data, usedforsecurity=usedforsecurity).hexdigest()  # type: ignore[call-arg]
 
 except TypeError:  # pragma: no cover
 
