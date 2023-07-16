9b2d75f382c7b44a7079a9d338eb96cfbba26c20
Viicos <65306057+Viicos@users.noreply.github.com>
2023-07-16 20:44:15 +0200

    Format

--- starlette/templating.py
+++ starlette/templating.py
@@ -115,7 +115,12 @@ class Jinja2Templates:
         **env_options: typing.Any,
     ) -> "jinja2.Environment":
         @pass_context
-        def url_for(context: typing.Dict[str, typing.Any], name: str, /, **path_params: typing.Any) -> URL:
+        def url_for(
+            context: typing.Dict[str, typing.Any],
+            name: str,
+            /,
+            **path_params: typing.Any,
+        ) -> URL:
             request = context["request"]
             return request.url_for(name, **path_params)
 
