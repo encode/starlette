f927fa8958d51a7b3e145d7add3913e58fbc70ec
Matthew Burkard <matthewjburkard@gmail.com>
2023-08-13 19:16:23 -0400

    test: update unit test to reflect `QueryParam` changes

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -262,16 +262,16 @@ def test_queryparams():
     assert "a" in q
     assert "A" not in q
     assert "c" not in q
-    assert q["a"] == "456"
-    assert q.get("a") == "456"
+    assert q["a"] == ["123", "456"]
+    assert q.get("a") == ["123", "456"]
     assert q.get("nope", default=None) is None
     assert q.getlist("a") == ["123", "456"]
     assert list(q.keys()) == ["a", "b"]
-    assert list(q.values()) == ["456", "789"]
-    assert list(q.items()) == [("a", "456"), ("b", "789")]
+    assert list(q.values()) == [["123", "456"], "789"]
+    assert list(q.items()) == [("a", ["123", "456"]), ("b", "789")]
     assert len(q) == 2
     assert list(q) == ["a", "b"]
-    assert dict(q) == {"a": "456", "b": "789"}
+    assert dict(q) == {"a": ["123", "456"], "b": "789"}
     assert str(q) == "a=123&a=456&b=789"
     assert repr(q) == "QueryParams('a=123&a=456&b=789')"
     assert QueryParams({"a": "123", "b": "456"}) == QueryParams(
