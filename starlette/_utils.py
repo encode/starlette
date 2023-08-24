f268950dea8b095d80b10971690441fdb9cf3a23
Alex Grönholm <alex.gronholm@nextday.fi>
2023-08-24 23:24:27 +0300

    Skip coverage for a line not run on AnyIO 3

--- starlette/_utils.py
+++ starlette/_utils.py
@@ -83,6 +83,6 @@ def collapse_excgroups() -> typing.Generator[None, None, None]:
     except BaseException as exc:
         if has_exceptiongroups:
             while isinstance(exc, BaseExceptionGroup) and len(exc.exceptions) == 1:
-                exc = exc.exceptions[0]
+                exc = exc.exceptions[0]  # pragma: no cover
 
         raise exc
