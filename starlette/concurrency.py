8a72f6337b0620d7ec9e9d89cf116055c9aa52a1
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-01-20 14:03:59 +0100

    fix linter

--- starlette/concurrency.py
+++ starlette/concurrency.py
@@ -16,7 +16,7 @@ P = ParamSpec("P")
 T = typing.TypeVar("T")
 
 
-async def run_until_first_complete(*args: tuple[typing.Callable | dict]) -> None:  # type: ignore[type-arg]  # noqa: E501
+async def run_until_first_complete(*args: tuple[typing.Callable, dict]) -> None:  # type: ignore[type-arg]  # noqa: E501
     warnings.warn(
         "run_until_first_complete is deprecated "
         "and will be removed in a future version.",
--- starlette/exceptions.py
+++ starlette/exceptions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import http
 import typing
 import warnings
