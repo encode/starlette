64b22b8572d82d1a0d87a2df5efd7771a6fb452f
Scirlat Danut <scirlatdanut@scirlats-mini.lan>
2024-03-03 18:59:24 +0200

    Small improvements

--- tests/types.py
+++ tests/types.py
@@ -1,6 +1,8 @@
-from typing import Dict, Optional, Protocol, Union
+from __future__ import annotations
 
-from httpx import Cookies, Headers
+from typing import Protocol
+
+import httpx
 
 from starlette.testclient import TestClient
 from starlette.types import ASGIApp
@@ -15,8 +17,7 @@ class TestClientFactory(Protocol):  # pragma: no cover
         base_url: str = "http://testserver",
         raise_server_exceptions: bool = True,
         root_path: str = "",
-        cookies: Optional[Union[Cookies, Dict[str, str]]] = None,
-        headers: Optional[Headers] = None,
+        cookies: httpx._types.CookieTypes | None = None,  # type: ignore
+        headers: dict[str, str] | None = None,
         follow_redirects: bool = True,
-    ) -> TestClient:
-        ...
+    ) -> TestClient: ...
