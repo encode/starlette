63e62c15eeedae39a988abfc65b59782e3444419
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-08-12 09:04:07 +0200

    Add most used objects to `__init__.py`

--- starlette/__init__.py
+++ starlette/__init__.py
@@ -1,1 +1,31 @@
 __version__ = "0.38.2"
+
+from .applications import Starlette
+from .middleware import Middleware
+from .responses import (
+    HTMLResponse,
+    JSONResponse,
+    PlainTextResponse,
+    RedirectResponse,
+    Response,
+    StreamingResponse,
+)
+from .routing import Mount, Route, Router, WebSocketRoute
+from .websockets import WebSocket
+
+__all__ = (
+    "__version__",
+    "Starlette",
+    "Middleware",
+    "Response",
+    "HTMLResponse",
+    "JSONResponse",
+    "PlainTextResponse",
+    "RedirectResponse",
+    "StreamingResponse",
+    "Route",
+    "Router",
+    "Mount",
+    "WebSocketRoute",
+    "WebSocket",
+)
