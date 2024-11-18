9dc9d2e92919306fae683746e38530ff55ed3092
Hao Guan <gh@raptium.net>
2024-11-19 03:28:43 +0800

    fix(testclient): exclude query sting from `raw_path` (#2716)
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- starlette/testclient.py
+++ starlette/testclient.py
@@ -281,7 +281,7 @@ class _TestClientTransport(httpx.BaseTransport):
             scope = {
                 "type": "websocket",
                 "path": unquote(path),
-                "raw_path": raw_path,
+                "raw_path": raw_path.split(b"?", 1)[0],
                 "root_path": self.root_path,
                 "scheme": scheme,
                 "query_string": query.encode(),
@@ -300,7 +300,7 @@ class _TestClientTransport(httpx.BaseTransport):
             "http_version": "1.1",
             "method": request.method,
             "path": unquote(path),
-            "raw_path": raw_path,
+            "raw_path": raw_path.split(b"?", 1)[0],
             "root_path": self.root_path,
             "scheme": scheme,
             "query_string": query.encode(),
