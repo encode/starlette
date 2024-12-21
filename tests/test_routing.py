4dc9881b3c88feb64e4e9956a266f04f53cd00bd
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-12-21 20:09:56 +0100

    revert formatting

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -883,11 +883,7 @@ class Endpoint:
             id="staticmethod",
         ),
         pytest.param(Endpoint(), "Endpoint", id="object"),
-        pytest.param(
-            lambda request: ...,  # pragma: no branch
-            "<lambda>",
-            id="lambda",
-        ),
+        pytest.param(lambda request: ..., "<lambda>", id="lambda"),  # pragma: no branch
     ],
 )
 def test_route_name(endpoint: typing.Callable[..., Response], expected_name: str) -> None:
