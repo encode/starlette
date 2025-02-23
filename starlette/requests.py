2fd523d06ac842f023dec5422e48cf61724416df
Mostafa Khalil <m.s.elkhadrawy@gmail.com>
2025-02-23 16:36:23 +0200

    feat: make form file memory size limit configurable

--- starlette/requests.py
+++ starlette/requests.py
@@ -255,6 +255,7 @@ class Request(HTTPConnection):
         max_files: int | float = 1000,
         max_fields: int | float = 1000,
         max_part_size: int = 1024 * 1024,
+        max_file_memory_size: int = 1024 * 1024,
     ) -> FormData:
         if self._form is None:  # pragma: no branch
             assert parse_options_header is not None, (
@@ -271,6 +272,7 @@ class Request(HTTPConnection):
                         max_files=max_files,
                         max_fields=max_fields,
                         max_part_size=max_part_size,
+                        spool_max_size=max_file_memory_size,
                     )
                     self._form = await multipart_parser.parse()
                 except MultiPartException as exc:
@@ -290,9 +292,15 @@ class Request(HTTPConnection):
         max_files: int | float = 1000,
         max_fields: int | float = 1000,
         max_part_size: int = 1024 * 1024,
+        max_file_memory_size: int = 1024 * 1024,
     ) -> AwaitableOrContextManager[FormData]:
         return AwaitableOrContextManagerWrapper(
-            self._get_form(max_files=max_files, max_fields=max_fields, max_part_size=max_part_size)
+            self._get_form(
+                max_files=max_files,
+                max_fields=max_fields,
+                max_part_size=max_part_size,
+                max_file_memory_size=max_file_memory_size,
+            )
         )
 
     async def close(self) -> None:
