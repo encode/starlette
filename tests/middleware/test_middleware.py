f76f829da943160506aa659d7295fc6e274d4dfa
alv2017 <v.alishauskaite@gmail.com>
2025-03-16 23:46:05 +0200

    refactoring: tests/middleware/test_middleware.py

--- tests/middleware/test_middleware.py
+++ tests/middleware/test_middleware.py
@@ -12,11 +12,21 @@ class CustomMiddleware:  # pragma: no cover
         await self.app(scope, receive, send)
 
 
-def test_middleware_repr() -> None:
-    middleware = Middleware(CustomMiddleware, "foo", bar=123)
-    assert repr(middleware) == "Middleware(CustomMiddleware, 'foo', bar=123)"
+class TestMiddleware:
+    custom_middleware = Middleware(CustomMiddleware, "foo", bar=123, baz="custom")
 
+    def test_created_instance(self) -> None:
+        custom_middleware = self.custom_middleware
+        assert isinstance(custom_middleware, Middleware)
+        assert custom_middleware.cls == CustomMiddleware
 
-def test_middleware_iter() -> None:
-    cls, args, kwargs = Middleware(CustomMiddleware, "foo", bar=123)
-    assert (cls, args, kwargs) == (CustomMiddleware, ("foo",), {"bar": 123})
+    def test_repr(self) -> None:
+        custom_middleware = self.custom_middleware
+        assert repr(custom_middleware) == "Middleware(CustomMiddleware, 'foo', bar=123, baz='custom')"
+
+    def test_iter(self) -> None:
+        custom_middleware = self.custom_middleware
+        cls, args, kwargs = custom_middleware
+        assert cls == custom_middleware.cls
+        assert args == ("foo",)
+        assert kwargs == {"bar": 123, "baz": "custom"}
